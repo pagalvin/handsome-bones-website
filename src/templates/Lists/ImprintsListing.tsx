367d6f70857cc8583fa0602e6cb3465a98be9c51
Paul Galvin <paul.galvin@neudesic.com>
2022-12-24 13:14:08 -0500

    Can pass in an href on vertical listings

--- src/templates/Lists/ImprintsListing.tsx
+++ src/templates/Lists/ImprintsListing.tsx
@@ -11,12 +11,14 @@ const ImprintsListing = () => (
       description="The Be Better imprint covers technology and consulting materials designed to help readers become better at their craft."
       image="/assets/images/feature.svg"
       imageAlt="Be Better"
+      linkToHref="/imprints/be-better"
     />
     <VerticalFeatureRow
       title="Cat's Claw"
       description="The Cat's Claw imprint includes a collection of fantasy novels and other whimsy."
       image="/assets/images/feature2.svg"
       imageAlt="Cat's Claws Imprint"
+      linkToHref="/imprints/cats-claw"
       reverse
     />
   </Section>
